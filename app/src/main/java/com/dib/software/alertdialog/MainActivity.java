00b62dda661b522a3e023b3123c3a79ca44ac897
dib LLC <89296160+dib-artificial@users.noreply.github.com>
2021-09-05 16:14:44 +0600

    Initial commit

---
+++ app/src/main/java/com/dib/software/alertdialog/MainActivity.java
@@ -0,0 +1,17 @@
+package com.dib.software.alertdialog;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
